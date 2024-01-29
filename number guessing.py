8eb368941583d4ec4828ee2bb78061af94e57ab0
Mahmoud Ebrahim <142084714+bandito19@users.noreply.github.com>
2024-01-29 11:54:05 +0200

    Tries
    Adding limit to the number of times the user can guess.

--- number guessing.py
+++ number guessing.py
@@ -3,14 +3,20 @@ from random import randint
 lower, upper = 1, 10
 random_number = randint(lower, upper)
 
-print("Guess a number in the range from {lower} to {upper}")
+tries = 3
+print(f"Guess a number in the range from {lower} to {upper}, You have {tries} tries")
 
 while True:
-    try:
-        user_input  = int(input("Guess: "))
-    except ValueError as e:
-        print("please enter a valid number.")
-        continue
+    if tries > 0:
+        try:
+            user_input  = int(input("Guess: "))
+            tries -= 1
+        except ValueError as e:
+            print("please enter a valid number.")
+            continue
+    else:
+        print("You ran out of tries")
+        break
 
     if user_input > random_number:
         print("Try a smaller number.")
