428197ddb9fbed727cabeaad1ac62414773bb175
Mahmoud Ebrahim <142084714+bandito19@users.noreply.github.com>
2024-02-13 17:54:55 +0200

    functions of the porgram

--- web_checker/web_checker.py
+++ web_checker/web_checker.py
@@ -2,3 +2,46 @@ import csv
 import requests
 from http import HTTPStatus
 from fake_useragent import UserAgent
+
+def get_websites(csv_path):
+    websites = []
+    with open (csv_path, 'r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            if "https://" not in row[1] or "http://" in row[1]:
+                websites.append(f"https://{row[1]}")
+            else :
+                websites.append(row[1])     
+    return websites
+
+
+def get_userAgent():
+    ua = UserAgent()
+    return ua.edge
+
+
+def description(code):
+    for value in HTTPStatus:
+        if value == code:
+            description = f'({value} {value.name}) {value.description}'
+            return description
+    return '(???) Unknown status code.'
+        
+
+def check_website(website, user_agent):
+    try:
+        code = requests.get(website, headers={'User-Agent': user_agent}).status_code
+        print(website, description(code))
+    except Exception:
+        print(f'Could not get information form website: {website}')
+
+
+def main():
+    sites = get_websites('websites.csv')
+    userAgent = get_userAgent()
+    for i, site in enumerate(sites):
+        check_website(site, userAgent)
+
+
+if __name__ =='__main__':
+    main()
