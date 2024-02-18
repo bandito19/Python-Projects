19a501439d1e7a5e6611e37e7717a05e0ac85511
Mahmoud Ebrahim <142084714+bandito19@users.noreply.github.com>
2024-02-18 13:20:17 +0200

    main function

--- password_Checker/main.py
+++ password_Checker/main.py
@@ -1,10 +1,18 @@
 def check_password(password):
-    with open('passwords.text', 'r') as file:
+    with open('passwords.txt', 'r') as file:
         common_passwrods = file.read().splitlines()
 
     for i, common_password in enumerate(common_passwrods, start=1):
         if password == common_password:
             print(f'{password}: ❌ (#{i})')
             return
-        print(f'{password}: ✅ (Unique)')
         
+    print(f'{password}: ✅ (Unique)')
+
+
+def main():
+    user_password = input('Enter a password: ')
+    check_password(user_password)
+
+if __name__ == '__main__':
+    main()
