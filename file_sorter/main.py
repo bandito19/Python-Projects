b71cfed52b7cdfdb78cc78933fc1cdd38e76dd46
Mahmoud Ebrahim <142084714+bandito19@users.noreply.github.com>
2024-04-13 14:54:19 +0200

    Update main.py

--- file_sorter/main.py
+++ file_sorter/main.py
@@ -5,9 +5,44 @@ def create_folder(path, extension):
     folder_name = extension[1:]
     folder_path = os.path.join(path, folder_name)
 
-    if not os.exists(folder_path):
+    if not os.path.exists(folder_path):
         os.makedirs(folder_path)
 
     return folder_path
 
 
+def sort_files(source_path):
+    for root_dir, sub_dir, filenames in os.walk(source_path):
+        for filename in filenames:
+            file_path = os.path.join(root_dir, filename)
+            extension = os.path.splitext(filename)[1]
+
+            if extension:
+                target_folder = create_folder(source_path, extension)
+                target_path = os.path.join(target_folder, filename)
+                shutil.move(file_path, target_path)
+
+
+
+def remove_empty_folders(source_path):
+    for root_dir, sub_dir, filenames in os.walk(source_path, topdown=False):
+        for current_dir in sub_dir:
+            folder_path = os.path.join(root_dir, current_dir)
+
+            if not os.listdir(folder_path):
+                os.rmdir(folder_path)
+
+
+
+def main():
+    user_input = input('file path: ')
+    if os.path.exists(path=user_input):
+        sort_files(user_input)
+        remove_empty_folders(user_input)
+        print('filse sorted successfully.')
+    else:
+        print('Invalid path.')
+
+
+if __name__=='__main__':
+    main()
