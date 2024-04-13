6201f67cd87d48183de68cb30e13b380723c655a
Mahmoud Ebrahim <142084714+bandito19@users.noreply.github.com>
2024-04-13 14:30:00 +0200

    creating folder method

--- file_sorter/main.py
+++ file_sorter/main.py
@@ -1,3 +1,13 @@
 import os
 import shutil
 
+def create_folder(path, extension):
+    folder_name = extension[1:]
+    folder_path = os.path.join(path, folder_name)
+
+    if not os.exists(folder_path):
+        os.makedirs(folder_path)
+
+    return folder_path
+
+
