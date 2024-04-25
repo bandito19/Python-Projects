a179fbcb086e7677782c9c93380beaf2090997e8
Mahmoud Ebrahim <142084714+bandito19@users.noreply.github.com>
2024-04-25 10:28:48 +0200

    importing issue

--- pdfReader/main.py
+++ pdfReader/main.py
@@ -1,10 +1,10 @@
 import re 
 from collections import Counter
-from pyPDF2 import pdfReader
+from PyPDF2 import PdfReader
 
 def extarct_text_from_pdf(pdf_file):
     with open(pdf_file, 'rb') as pdf:
-        reader = pdfReader(pdf_file, strict=False)
+        reader = PdfReader(pdf_file, strict=False)
         print('Pages: ', len(reader.pages))
         print('-'*10)
         return [page.extract_text() for page in reader.pages]
